7738d93cc858cb4dd8cb0ba0ae38e3b6f98aff26
Willhelm Hawk <uno.yakshi@gmail.com>
2023-12-03 11:28:39 +0000

    feat(ci): Added back-end target deployment job into .space.kts...

--- .space.kts
+++ .space.kts
@@ -5,5 +5,15 @@
 */
 
 job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+    container("amazoncorretto:17-alpine") {
+      kotlinScript { api ->
+          api.space().projects.automation.deployments.start(
+              project = api.projectIdentifier(),
+              targetIdentifier = TargetIdentifier.Key("digital-ocean-back-end"),
+              version = "1.0.0",
+              // automatically update deployment status based on a status of a job
+              syncWithAutomationJob = true
+          )
+      }
+  }
 }
