64debf72c38580a498f14cf55a3e0e5a58de5612
UnoYakshi <uno.yakshi@gmail.com>
2023-12-03 10:55:17 +0300

    refactor(html): Added search pagination
    - Added enumerate for Jinja2
    - Added skip+limit pagination for search

--- src/fraand_core/domains/search/router.py
+++ src/fraand_core/domains/search/router.py
@@ -6,9 +6,13 @@ Endpoints for search domain.
 
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.fraand_core.deps import get_async_session
+from src.fraand_core.domains.items.dependencies import search_query
+from src.fraand_core.domains.items.routers.crud import get_items
 from src.fraand_core.domains.users.dependencies import current_active_user
 from src.fraand_core.domains.users.models import User
 from src.fraand_core.templates import app_templates
@@ -16,20 +20,29 @@ from src.fraand_core.templates import app_templates
 search_router = APIRouter()
 
 
-@search_router.get('/search/', response_class=HTMLResponse)
-async def get_search_results(query: str, user: Annotated[User | None, Depends(current_active_user)]) -> HTMLResponse:
-    """Return search result for the user query."""
-    items = [
-        {
-            'id': '05ee38de-76c2-4751-bb34-d579237c5a9e',
-            'name': 'Very Cool Tool',
-            'description': 'Tis but a nice thing to share amongst your kin!',
-            'city': 'London',
-            'owner_id': '25321f69-3162-47ae-8aa9-5fa5f559beaa',
-        },
-    ]
+@search_router.get('/', response_class=HTMLResponse)
+async def get_search_results(
+    search_params: Annotated[dict, Depends(search_query)],
+    request: Request,
+    user: Annotated[User | None, Depends(current_active_user)],
+    session: AsyncSession = Depends(get_async_session),
+) -> HTMLResponse:
+    """Return Items search result for the user query..."""
 
-    context = {'query': query, 'items': items}
+    items = await get_items(
+        search_params=search_params,
+        should_search_in_name=True,
+        session=session,
+    )
+
+    context = {
+        'request': request,
+        'q': search_params['q'],
+        'skip': search_params.get('skip'),
+        'limit': search_params.get('limit'),
+        'items': items,
+    }
     if user:
         context['current_user'] = user
+
     return app_templates.TemplateResponse('widgets/search/results.html', context=context)
--- src/fraand_core/templates/__init__.py
+++ src/fraand_core/templates/__init__.py
@@ -1,8 +1,18 @@
 """HTML-templates module, based on Jinja2 (somewhat was ported from Django)..."""
 
+from typing import Any, Collection
 
 from fastapi.templating import Jinja2Templates
 
 from src.fraand_core.constants import TEMPLATES_ABS_FILE_PATH
 
 app_templates = Jinja2Templates(directory=TEMPLATES_ABS_FILE_PATH, auto_reload=True)
+
+
+def jinja2_filter_enumerate(sequence: Collection) -> Collection[tuple[int, Any]]:
+    """Simple ``enumerate()`` for Jinja2..."""
+
+    return zip(range(0, len(sequence)), sequence, strict=True)
+
+
+app_templates.env.filters['enumerate'] = jinja2_filter_enumerate
